f6a7957287a2ebcf9306f639d3464ca56b3881fa
17pdm1 <17pdm1@queensu.ca>
2020-02-01 21:53:26 -0500

    some cool things

--- qhacks2020/lib/views/home_view.dart
+++ qhacks2020/lib/views/home_view.dart
@@ -4,11 +4,6 @@ import '../tools/responsive_builder.dart';
 import './home_view_mobile.dart';
 import './home_view_desktop.dart';
 
-final List<String> names = <String>['The Tea Room', 'Starbucks', 'Tim Hortons'];
-final List<String> description = <String>['A cool EngSoc coffee shop', 'Fancy corporate coffee', 'Cheap corporate coffee'];
-final List<String> pics = <String>['assets/images/tearoom.jfif', 'assets/images/starbucks.png', 'assets/images/timhortons.jfif'];
-final List<int> ratings = <int>[5, 2, 2];
-
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- qhacks2020/lib/views/home_view_desktop.dart
+++ qhacks2020/lib/views/home_view_desktop.dart
@@ -4,6 +4,12 @@ import 'package:qhacks2020/widgets/navigation_bar/navigation_bar.dart';
 import 'package:qhacks2020/widgets/centered_view/centered_view.dart';
 import 'package:qhacks2020/widgets/call_to_action/call_to_action.dart';
 
+
+final List<String> names = <String>['The Tea Room', 'Starbucks', 'Tim Hortons'];
+final List<String> description = <String>['A cool EngSoc coffee shop', 'Fancy corporate coffee', 'Cheap corporate coffee'];
+final List<String> pics = <String>['assets/images/tearoom.jfif', 'assets/images/starbucks.png', 'assets/images/timhortons.jfif'];
+final List<int> ratings = <int>[5, 2, 2];
+
 class HomeViewDesktop extends StatelessWidget {
   const HomeViewDesktop({Key key}) : super(key: key);
 
@@ -44,6 +50,18 @@ class HomeViewDesktop extends StatelessWidget {
             ],
           ),
           ),
+          Container(
+          width: 400,
+          child: ListView.separated(
+            scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.all(8),
+            itemCount: names.length,
+            itemBuilder: (BuildContext context, int index) {
+              return BusinessCard(text1: names[index], text2: description[index], rating: ratings[index], filepath: pics[index]);
+            },
+            separatorBuilder: (BuildContext context, int index) => const Divider(),
+          ),
+        ),
         ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
